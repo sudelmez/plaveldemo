32494d7ec752cf95b5d7504c3b08d2efb5ec5198
Sude Ölmez <98457237+sudelmez@users.noreply.github.com>
2023-07-19 13:54:49 +0300

    setstate

--- src/component/user/user.js
+++ src/component/user/user.js
@@ -1,4 +1,4 @@
-import { React,Component } from "react";
+import { React,Component,TouchableOpacity } from "react";
 import { View, Text } from 'react-native';
 
 // import PropTypes from 'prop-types';
@@ -21,14 +21,25 @@ constructor(props){  //initstate işlemi denebilir
 // state = {  //böyle de state oluşturulabilir
 //     isVisible: true
 // }
+    // constructor(e){
+    //     super(props)
+    //     this.onPress = this.onPress.bind(this) //şeklinde de kullanılabilir bind
+    // }
+    onPress(e){
+        console.log(this);
+        this.setState({
+            isVisible: !isVisible  //fonksiyon her çağırılıdığında isvisible değeri değişir ve tekrar render edilir
+        })
+    }
 
     render(){
         const {name,number,email} =this.props;
         const {isVisible} =this.state;
         return (
-            <View>
-                isVisible ? <Text>{this.state.test}</Text> : null
+            <View> 
+                isVisible ? <TouchableOpacity onPress= {this.onPress.bind(this)}> <Text>{this.state.test}</Text> </TouchableOpacity>  : null
             </View>
+            //bind fonksiyonu, yazılan fonksiyonun classını tanımasını sağlar. yani olmadığında null dönerken, olduğunda user objesini gösterir.
 
             //ilgili kısımda {this.state.test} diyerek state elemanına ulaşılabilir
         );
